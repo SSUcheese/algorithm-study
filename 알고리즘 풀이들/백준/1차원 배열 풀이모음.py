ec66d2fb0c463962ae59983ccd3297304d57f04c
cheesemani <zoojj3319@gmail.com>
2022-01-01 23:02:50 +0900

    2022/01/01 study

--- 알고리즘 풀이들/백준/1차원 배열 풀이모음.py
+++ 알고리즘 풀이들/백준/1차원 배열 풀이모음.py
@@ -52,3 +52,34 @@ for i in range(10):
     a.append(n)
 a = set(a)
 print(len(a))'''
+
+
+#1546번
+'''num = int(input())
+a = list(map(int, input().split()))
+b = []
+mean = 0
+
+for i in range(0, num, 1):
+    b.append(a[i] / max(a) * 100) 
+
+mean = sum(b) / len(b)
+print(mean)'''
+
+#8958번
+
+'''num = int(input())
+
+
+for i in range(num):
+    score = 0
+    total = 0
+    a = input()
+    
+    for h in range(0, len(a), 1):
+        if a[h] == 'O':
+            score += 1
+            total = total + score
+        elif a[h] == 'X':
+            score = 0
+    print(total)'''
