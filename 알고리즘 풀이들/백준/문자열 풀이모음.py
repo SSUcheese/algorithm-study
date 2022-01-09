af54b8b35928f55ee083ff04b966f3bdac20a1c5
cheesemani <zoojj3319@gmail.com>
2022-01-09 19:53:57 +0900

    01/08 study. Forgot to upload it yesterday

---
+++ 알고리즘 풀이들/백준/문자열 풀이모음.py
@@ -0,0 +1,17 @@
+#11654번
+
+'''num = input()
+print(ord(num))'''
+
+
+#11720번
+
+'''a= input()
+num = input()
+add = 0
+c = 0
+
+for i in range(0, len(num), 1):
+    c = int(num[i])
+    add += c
+print(add)'''   
