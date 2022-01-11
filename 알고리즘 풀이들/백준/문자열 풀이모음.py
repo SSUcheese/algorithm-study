d3d02e5513fced936dad7748e4bf5b5db2f61613
cheesemani <zoojj3319@gmail.com>
2022-01-11 23:52:06 +0900

    01/10 ,01/11 study

--- 알고리즘 풀이들/백준/문자열 풀이모음.py
+++ 알고리즘 풀이들/백준/문자열 풀이모음.py
@@ -15,3 +15,26 @@ for i in range(0, len(num), 1):
     c = int(num[i])
     add += c
 print(add)'''   
+
+#10809번
+
+'''abc= 'abcdefghijklmnopqrstuvwxyz'
+
+word = input()
+
+for i in abc:
+    if i in word:
+        print(word.index(i), end=' ')
+    else:
+        print(-1, end=' ')'''
+        
+#2675번
+
+'''repeat_num = int(input())
+
+for i in range(repeat_num):
+    r1, word1 = input().split()
+    r1=int(r1)
+    for i in word1:
+        print(f'{i*r1}', end='')
+    print(end='\n')'''
