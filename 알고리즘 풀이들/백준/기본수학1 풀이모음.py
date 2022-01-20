b2c556bd86a0585332d36fad1bd88b759047f1b5
cheesemani <zoojj3319@gmail.com>
2022-01-20 17:56:57 +0900

    01/20 study

---
+++ 알고리즘 풀이들/백준/기본수학1 풀이모음.py
@@ -0,0 +1,20 @@
+#1712번
+
+'''a, b, c = map(int, input().split())
+i = 0
+
+if c > b:
+    while (c * i) < (a + (b * i)):
+        i += 1
+    print(i+1)
+else:
+    print(-1)''' #이런식으로 풀면 시간초과로 탈락임
+    
+#간단한 방정식을 구성해서 푸는 방식이 효율적이고 빠르다  
+
+'''a, b, c = map(int, input().split())
+
+if c> b:
+    print(int(a/(c-b)+1))
+else:
+    print(-1) '''
